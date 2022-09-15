c456fface5af81ade9cfc7c8d9fe0496a6b176aa
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-15 13:34:03 +0100

    wrote questions in array of object

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-var questionBank = []
+var questionBank = [
+    {
+        question : 'Eritrea, which became the 182nd member of the UN in 1993,is in the continent of',
+        option : ['Asia','Africa','Europe','Nigeria'],
+        answer : 'Africa'
+    },
+    {
+        question : 'Garampani sanction is located at',
+        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
+        answer : 'Diphu, Assam'
+    },
+    {
+        question : '',
+        option : ['','','',''],
+        answer : ''
+    },
+    {
+        question : '',
+        option : ['','','',''],
+        answer : ''
+    },
+    {
+        question : '',
+        option : ['','','',''],
+        answer : ''
+    }
+]
