82c2242460362be52acc09041c720547a90df0e0
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-15 16:34:37 +0100

    all working with little bug

--- script.js
+++ script.js
@@ -51,7 +51,8 @@ const displayQuestion = ()=>{
     stat.innerHTML = "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
 }
 
-const calscore = (e)=>{
+//function to calculate scores
+const calcScore = (e)=>{
     if(e.innerHTML===questionBank[i].answer && score<questionBank.length){
         score = score+1;
         document.getElementById(e.id).style.background = 'limegreen';
@@ -69,7 +70,8 @@ const nextQuestion = ()=>{
     }
     else{
         points.innerHTML = score+'/'+questionBank.length;
-        quizContainer.style.display = 'block'
+        quizContainer.style.display = 'none';
+        scoreboard.style.display = 'block'
     }
 }
 
@@ -86,7 +88,7 @@ const checkAnswer = ()=>{
     var answerBank = document.getElementById('answerBank');
     var answers = document.getElementById('answers');
     answerBank.style.display = 'block';
-    // scoreboard.style.display = 'block';
+    // scoreboard.style.display = 'none';
     for(var a=0; a<questionBank.length; a++){
         var list = document.createElement('li');
         list.innerHTML = questionBank[a].answer;
