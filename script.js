197fd504e5f7b6c054c6642e6790c9bf238ddc96
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-15 14:04:29 +0100

    done with the quetions and answers

--- script.js
+++ script.js
@@ -10,18 +10,19 @@ var questionBank = [
         answer : 'Diphu, Assam'
     },
     {
-        question : '',
-        option : ['','','',''],
-        answer : ''
+        question : 'For which of the following disiplines is Nobel Prize awarded?',
+        option : ['Phyiscs and Chemistry','Physiology or Medical','Literature, Peace and Economics','All of the above'],
+        answer : 'All of the above'
     },
     {
-        question : '',
-        option : ['','','',''],
-        answer : ''
+        question : 'Hitler party which came into power in 1933 is known as',
+        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
+        answer : 'Nazi Party'
     },
     {
-        question : '',
-        option : ['','','',''],
-        answer : ''
+        question : 'First human heart transplanted operation conducted br Dr. Washkanshy, was conducted in',
+        option : ['1967','1968','1958','1922'],
+        answer : '1967'
     }
 ]
+
