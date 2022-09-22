92acfca269b3a008bd98267de2bd515789a1e6c7
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-22 12:59:22 +0100

    still working

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ var next = document.querySelector('.next');
 var points = document.getElementById('score');
 var span = document.querySelectorAll('span');
 var i = 0;
-var score = 0;
+var score = 500;
 
 const displayQuestion = ()=>{
     for(var a=0; a<span.length; a++){
@@ -48,13 +48,13 @@ const displayQuestion = ()=>{
     option1.innerHTML = questionBank[i].option[1];
     option2.innerHTML = questionBank[i].option[2];
     option3.innerHTML = questionBank[i].option[3];
-    stat.innerHTML = "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+    // stat.innerHTML = "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
 }
 
 //function to calculate scores
 const calcScore = (e)=>{
-    if(e.innerHTML === questionBank[i].answer && score < questionBank.length){
-        score = score+1;
+    if(e.innerHTML === questionBank[i].answer && score > questionBank.length){
+        score += 500;
         document.getElementById(e.id).style.background = 'green';
     }
     else{
@@ -69,7 +69,7 @@ const nextQuestion = ()=>{
         displayQuestion();
     }
     else{
-        points.innerHTML = score+'/'+questionBank.length;
+        points.innerHTML = `Congratulation youve earned $ ${score}`;
         quizContainer.style.display = 'none';
         scoreboard.style.display = 'block'
     }
